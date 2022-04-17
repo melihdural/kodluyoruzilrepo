7dafd2954bfea1ef54fe3c83b3ba1c313c6d8f3b
Melih Dural <duralmelih@gmail.com>
2022-04-17 04:46:14 +0300

    İkinci ders ödevi tamamlandı.

--- cSharpProgramlamaEgitim/Program.cs
+++ cSharpProgramlamaEgitim/Program.cs
@@ -6,6 +6,18 @@ namespace Csharpprogramlama
     {
         static void Main(string[] args)
         {
+            IlkDers();
+            Console.WriteLine("İlk dersin sonu");
+            DegiskenlerDersiOdev();
+            Console.WriteLine("İkinci dersin sonu");
+            
+            
+            Console.ReadKey();
+        }
+
+        private static void IlkDers()
+        {
+            //C# İlk Ders
             Console.WriteLine("Hello World!");
             Console.WriteLine("İsminizi girin:");
             string name = Console.ReadLine();
@@ -13,7 +25,20 @@ namespace Csharpprogramlama
             string surname = Console.ReadLine();
             Console.WriteLine("Merhaba " + name + " " + surname);
 
-            Console.ReadKey();
         }
+
+        
+        private static void DegiskenlerDersiOdev()
+        {
+            //HackerRank sitesindeki ödevin bir örneği
+            Console.WriteLine("Lütfen, 'Welcome to 30 Days of Code!' yazınız.");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(inputString);
+    
+
+        }
+
     }
+
 }
