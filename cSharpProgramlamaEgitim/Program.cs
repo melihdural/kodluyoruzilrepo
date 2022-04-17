ce7281b028e04b5551c1bf69e60f5912cc5f6ae5
Melih Dural <duralmelih@gmail.com>
2022-04-17 04:32:23 +0300

    C# Eğitimine başlandı. Eğitimle ilgili gerekli dosyalar oluşturuldu.

---
+++ cSharpProgramlamaEgitim/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Csharpprogramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+
+            Console.ReadKey();
+        }
+    }
+}
