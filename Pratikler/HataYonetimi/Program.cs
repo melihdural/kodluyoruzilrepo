de267e8effe343f08e4bd1b5a518dde1cf313e3c
Melih Dural <duralmelih@gmail.com>
2022-04-21 02:17:50 +0300

    Hata-Yönetimi dersi tamamlandı.

---
+++ Pratikler/HataYonetimi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Yaşınızı giriniz: ");
+                int yasiniz = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Yaşınız: " + yasiniz);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("FormatException: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşleminiz tamamlandı");
+            }
+
+            Console.ReadKey();
+        
+        }
+    }
+}
