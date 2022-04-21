41658fd70581caf1349ce744fe8d7030b9094e93
Melih Dural <duralmelih@gmail.com>
2022-04-22 00:42:02 +0300

    HackerRank forLoop Challange

---
+++ Pratikler/ForLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basamak oluşturmak için bir sayı giriniz!");
+            int n = int.Parse(Console.ReadLine());
+            StairCase(n);
+
+            Console.ReadKey();
+        }
+
+        private static void StairCase(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('#', i+1).PadLeft(n,' '));
+            }
+        }
+    }
+}
