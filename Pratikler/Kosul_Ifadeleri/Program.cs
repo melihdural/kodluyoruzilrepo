d3654b882bdfcf5a67299806e9c2566daf307b45
Melih Dural <duralmelih@gmail.com>
2022-04-21 03:38:32 +0300

    SwitchCase koşulları eklendi

--- Pratikler/Kosul_Ifadeleri/Program.cs
+++ Pratikler/Kosul_Ifadeleri/Program.cs
@@ -7,8 +7,129 @@ namespace Kosul_Ifadeleri
         static void Main(string[] args)
         {
             IfElse();
+            SwitchCase();
 
             Console.ReadKey();
+        }
+
+        private static void SwitchCase()
+        {
+            int month = DateTime.Now.Month;
+            int season = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                Console.WriteLine("Ocak ayındasınız");
+                break;
+
+                case 2:
+                Console.WriteLine("Şubat ayındasınız");
+                break;
+
+                case 3:
+                Console.WriteLine("Mart ayındasınız");
+                break;
+
+                case 4:
+                Console.WriteLine("Nisan ayındasınız");
+                break;
+
+                case 5:
+                Console.WriteLine("Mayıs ayındasınız");
+                break;
+
+                case 6:
+                Console.WriteLine("Haziran ayındasınız");
+                break;
+
+                case 7:
+                Console.WriteLine("Temmuz ayındasınız");
+                break;
+
+                case 8:
+                Console.WriteLine("Ağustos ayındasınız");
+                break;
+
+                case 9:
+                Console.WriteLine("Eylül ayındasınız");
+                break;
+
+                case 10:
+                Console.WriteLine("Ekim ayındasınız");
+                break;
+
+                case 11:
+                Console.WriteLine("Kasım ayındasınız");
+                break;
+
+                case 12:
+                Console.WriteLine("Aralık ayındasınız");
+                break;
+
+                default:
+                Console.WriteLine("Tarih Okunamadı.");
+                break;
+
+            }
+
+            switch(season)
+            {
+                case 12:
+                Console.WriteLine("Kış Mevsimindesiniz");
+                break;
+
+                case 1:
+                Console.WriteLine("Kış Mevsimindesiniz");
+                break;
+
+                case 2:
+                Console.WriteLine("Kış Mevsimindesiniz");
+                break;
+
+                case 3:
+                Console.WriteLine("Bahar Mevsimindesiniz");
+                break;
+
+                case 4:
+                Console.WriteLine("Bahar Mevsimindesiniz");
+                break;
+
+                case 5:
+                Console.WriteLine("Bahar Mevsimindesiniz");
+                break;
+
+                case 6:
+                Console.WriteLine("Yaz Mevsimindesiniz");
+                break;
+
+                case 7:
+                Console.WriteLine("Yaz Mevsimindesiniz");
+                break;
+
+                case 8:
+                Console.WriteLine("Yaz Mevsimindesiniz");
+                break;
+
+                case 9:
+                Console.WriteLine("Sonbahar Mevsimindesiniz");
+                break;
+
+                case 10:
+                Console.WriteLine("Sonbahar Mevsimindesiniz");
+                break;
+
+                case 11:
+                Console.WriteLine("Sonbahar Mevsimindesiniz");
+                break;
+
+                default:
+                Console.WriteLine("Tarih Okunamadı.");
+                break;
+
+            }
+
+
         }
 
         private static void IfElse()
