017701cc02dd3fb7dbe7208c6ee657ba47d8d302
Melih Dural <duralmelih@gmail.com>
2022-04-22 01:41:43 +0300

    While&ForEach Loops

---
+++ Pratikler/ForEach_WhileLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ForEach_WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***While***");
+            float sayi1 = float.Parse(Console.ReadLine());
+            float sayac = 1;
+            float toplam = 0;
+            while (sayac<=sayi1)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.Write(toplam/sayi1);
+
+            Console.WriteLine("***ForEach***");
+            string[] mevsimler = {"ilkbahar","yaz","sonbahar","kış"};
+
+            foreach (string m in mevsimler)
+            {
+                string mevsim = m;
+                Console.WriteLine(mevsim);
+            }
+
+            Console.ReadKey();
+                   
+        }
+    }
+}
