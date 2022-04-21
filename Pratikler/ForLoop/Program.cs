ef39296f9d4756ce470d5d4b70ec0552667b4946
Melih Dural <duralmelih@gmail.com>
2022-04-22 00:52:49 +0300

    updated forLoop Task

--- Pratikler/ForLoop/Program.cs
+++ Pratikler/ForLoop/Program.cs
@@ -17,7 +17,20 @@ namespace ForLoop
         {
             for (int i = 0; i < n; i++)
             {
+                
                 Console.WriteLine(new string('#', i+1).PadLeft(n,' '));
+                
+                if(i==3)
+                {
+                    Console.WriteLine("i=3");
+                    continue;
+                }
+
+                if(i>6)
+                {
+                    Console.WriteLine("i=6 loop concluded by break;");
+                    break;
+                }
             }
         }
     }
