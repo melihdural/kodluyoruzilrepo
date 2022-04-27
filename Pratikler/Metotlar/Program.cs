932173b837164092ed27549059e69053439ef32a
Melih Dural <duralmelih@gmail.com>
2022-04-27 20:43:34 +0300

    Metotlar ders örnekleri eklendi

---
+++ Pratikler/Metotlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 8;
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+            ornek.EkranaYazdir(Convert.ToString(ornek.ArttirVeTopla(a,b)));
+
+            Console.ReadKey();
+        }
+
+        static int Topla(int deger1 , int deger2)
+        {
+            return deger1+deger2;
+        }
+
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(int deger1, int deger2)
+        {
+            deger1++;
+            deger2++;
+            return deger1+deger2;
+        }
+    }
+}
