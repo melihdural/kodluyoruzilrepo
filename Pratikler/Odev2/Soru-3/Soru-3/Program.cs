1f9d60b301432d13f7c75e597ced65f198b4c403
melihdural <duralmelih@gmail.com>
2022-06-16 15:08:02 +0300

    Odev-2/Soru-3 tamamlandı.

--- Pratikler/Odev2/Soru-3/Soru-3/Program.cs
+++ Pratikler/Odev2/Soru-3/Soru-3/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections;
 
-Console.WriteLine("Hello, World!");
+namespace Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string vowels = "aeıioöuü";
+            ArrayList matchingLetters = new ArrayList();
+            Console.WriteLine("Bir cümle giriniz;");
+            text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if (text[i] == vowels[j])
+                    {
+                        matchingLetters.Add(text[i]);
+                    }
+                }
+            }
+
+            ArrayList repeatedVowels = new ArrayList();
+            foreach (var mL in matchingLetters)
+            {
+                if (repeatedVowels.Contains(mL))
+                {
+                    continue;
+                }
+                repeatedVowels.Add(mL);
+            }
+
+            foreach (var item in repeatedVowels)
+            {
+                Console.Write("Cümledeki sesli harfler: " + item + " ");
+            }        
+
+        }
+        
+    }
+}
